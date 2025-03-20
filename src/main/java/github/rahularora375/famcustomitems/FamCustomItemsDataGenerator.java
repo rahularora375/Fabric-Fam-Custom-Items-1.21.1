d95aa54588c257ea1d51d32a326ae18e08cc480e
rahularora375 <aroora.rahul@gmail.com>
2025-03-20 21:56:44 +0530

    Added comment

--- src/main/java/github/rahularora375/famcustomitems/FamCustomItemsDataGenerator.java
+++ src/main/java/github/rahularora375/famcustomitems/FamCustomItemsDataGenerator.java
@@ -3,6 +3,7 @@ package github.rahularora375.famcustomitems;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+// very important comment
 public class FamCustomItemsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
